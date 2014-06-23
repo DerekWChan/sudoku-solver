b9fc7de52b8b3e5dacbe2f4c39c170706926efc4
alfredababio1 <alfred.ababio@cengage.com>
2014-06-23 14:14:08 -0400

    Set up life for everyone

--- scriptjs.js
+++ scriptjs.js
@@ -1,3 +1,15 @@
+$(function() {
+	var solveButton = $('#solve');
+	//solveButton.html('Solve');
+	solveButton.bind('click', function() {
+		solve();
+	})
+});
+
 function convertFromStringToDOM() {
 	var uglyStringOfNumbers = $('pre').text();
+}
+
+function solve() {
+	alert('Brendan Sucks');
 }
